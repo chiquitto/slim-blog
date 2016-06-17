b26737d48900bcbbeb769e5ca1ab76f969e979c3
Alisson Chiquitto <chiquitto@gmail.com>
2016-06-16 23:29:17 -0300

    Trabalhando com parametros

--- src/middleware.php
+++ src/middleware.php
@@ -2,7 +2,3 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
-
-$app->add(function() {
-
-});
--- src/routes.php
+++ src/routes.php
@@ -4,12 +4,23 @@
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-
-
 $app->get('/posts', function(ServerRequestInterface $request, ResponseInterface $response, $args){
     $tab = new Application_Model_DbTable_Post();
-
     $posts = $tab->fetchAll();
 
-    // $response->getBody()->write(json_encode($posts->toArray()));
+    return $response->withJson($posts->toArray());
+});
+
+$app->get('/post/{id}', function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
+    $tab = new Application_Model_DbTable_Post();
+    $post = $tab->fetchRow("idpost = " . $args['id']);
+    
+    if (!$post) {
+        $response->withStatus(404);
+        return $response->withJson(array(
+            'msg' => 'Post inexistente'
+        ));
+    }
+
+    return $response->withJson($post->toArray());
 });
